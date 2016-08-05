47a5e4ea3177d1e4c5db8368c04e6d0d7f19afb9
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2016-08-05 16:39:05 -0700

    Add default gem

---
+++ lib/kube/templates.rb
@@ -0,0 +1,7 @@
+require "kube/templates/version"
+
+module Kube
+  module Templates
+    # Your code goes here...
+  end
+end
---
+++ lib/kube/templates/version.rb
@@ -0,0 +1,5 @@
+module Kube
+  module Templates
+    VERSION = "0.1.0"
+  end
+end
